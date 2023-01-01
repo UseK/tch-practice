9ca823adeb7942ceff060e5bc35449a4b36e9434
UseK <use.k.james@gmail.com>
2023-01-01 12:51:49 +0900

    Introduce tch and play with sample code at https://github.com/LaurentMazare/tch-rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
-fn main() {
-    println!("Hello, world!");
+use tch::nn::{Module, OptimizerConfig};
+use tch::{kind, nn, Device, Tensor};
+
+fn my_module(p: nn::Path, dim: i64) -> impl nn::Module {
+    let x1 = p.zeros("x1", &[dim]);
+    let x2 = p.zeros("x2", &[dim]);
+    nn::func(move |xs| {
+        println!("x1: {:#?}", x1);
+        println!("x2: {:#?}", x2);
+        xs * &x1 + xs.exp() * &x2
+    })
+}
+
+#[allow(dead_code)]
+fn gradient_descent() {
+    let vs = nn::VarStore::new(Device::Cpu);
+    let my_module = my_module(vs.root(), 7);
+    let mut opt = nn::Sgd::default().build(&vs, 1e-2).unwrap();
+    for _idx in 1..50 {
+        let xs = Tensor::zeros(&[7], kind::FLOAT_CPU);
+        let ys = Tensor::zeros(&[7], kind::FLOAT_CPU);
+        let loss = (my_module.forward(&xs) - ys)
+            .pow_tensor_scalar(2)
+            .sum(kind::Kind::Float);
+        opt.backward_step(&loss);
+        println!("{:#?}", opt);
+        println!("{:#?}", loss);
+    }
+}
+
+fn gradient_descent_from_raw(xs: &Tensor, ys: &Tensor, epochs: u32) -> impl nn::Module {
+    let vs = nn::VarStore::new(Device::Cpu);
+    let my_module = my_module(vs.root(), 7);
+    let mut opt = nn::Sgd::default().build(&vs, 1e-2).unwrap();
+    for epoch in 1..=epochs {
+        println!("\nepoch: {}/{}", epoch, epochs);
+        let loss = (my_module.forward(xs) - ys)
+            .pow_tensor_scalar(2)
+            .sum(kind::Kind::Float);
+        println!("loss: {:#?}", loss);
+        opt.backward_step(&loss);
+    }
+    my_module
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use tch::{nn::Module, Tensor};
+
+    use crate::gradient_descent_from_raw;
+
+    #[test]
+    fn test() {
+        let xs = Tensor::of_slice(&[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]);
+        let ys = Tensor::of_slice(&[0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0]);
+        let trained = gradient_descent_from_raw(&xs, &ys, 200);
+        let forwarded = trained.forward(&xs);
+        let expected = Tensor::of_slice(&[
+            0.5894472002983093,
+            0.4965878681698622,
+            0.39920678950967375,
+            0.2998769086883365,
+            0.19998866544379637,
+            0.09999951477228386,
+            0.0,
+        ]);
+        assert_eq!(forwarded, expected);
+    }
 }
