a2c0d2c5061b23b4583bf8bd11149fab636b7d22
UseK <use.k.james@gmail.com>
2023-03-05 15:15:21 +0900

    Just start to try CNN for loaded images

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use log::info;
+use tch_practice::{train, Net};
 
 fn main() -> Result<()> {
     run()
@@ -11,9 +12,12 @@ fn run() -> Result<()> {
     let m = tch_practice::data_loader::load_datasets(
         "data/mydata/images",
         "data/mydata/annotation_0_1.csv",
-        Some(|t: tch::Tensor| t.flatten(1, 3).internal_cast_float(true)),
+        // Some(|t: tch::Tensor| t.flatten(1, 3).internal_cast_float(true)),
+        Some(|t: tch::Tensor| t.internal_cast_float(true)),
     )?;
-    let model = tch_practice::run_adam(&m, 2)?;
+    // let model = tch_practice::run_adam(&m, 2)?;
+
+    let model = train(&m, Net::new, tch::nn::Adam::default(), tch::Device::Cpu, 20);
 
     info!("{:#?}", model);
     Ok(())
